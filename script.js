2e5f7fdd06a6655be7b6cb944830a38f761b118d
Ismail Adebayo <ismailadebayo94@gmail.com>
2024-08-05 23:27:30 +0100

    working on responsivness

---
+++ script.js
@@ -0,0 +1,15 @@
+// Script for navigation bar
+const ham = document.getElementById('ham')
+const nav = document.getElementById('nav')
+const close = document.getElementById('close')
+
+
+if(ham){
+    ham.addEventListener('click', ()=>{
+    nav.classList.add('active')
+})
+}
+
+close.addEventListener('click', ()=>{
+    nav.classList.remove('active')
+})
